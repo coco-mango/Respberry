8fccf3caa687868f02ecc0fe498f189f4b1ef7f8
yeju jo <154478990+coco-mango@users.noreply.github.com>
2024-04-05 15:47:20 +0900

    Update JYJmission01.py

--- test/JYJmission01.py
+++ test/JYJmission01.py
@@ -1,31 +1,32 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO                    # RPi.GPIO는 RaspberryPi의 GPIO 핀을 제어하기 위한 파이썬 라이브러리
 import time
 
 # GPIO 핀 번호 설정
-led_red =4 
-led_green=5
-led_blue=6    
+led_red =4                                 # RED LED의 GPIO 핀 번호 정의
+led_green=5                                # GREEN LED의 GPIO 핀 번호 정의
+led_blue=6                                 # BLUE LED의 GPIO 핀 번호 정의
 
-color_led = [led_red, led_green, led_blue]
+color_led = [led_red, led_green, led_blue] # LED의 GPIO 핀 번호를 리스트에 정의
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)                    # 경고 발생하면 무시
+GPIO.setmode(GPIO.BCM)                     # GPIO 핀 번호를 BCM 모드로 설정
+
 for pin in color_led:
-    GPIO.setup(pin, GPIO.OUT)          # 각 LED핀을 출력으로 설정              
+    GPIO.setup(pin, GPIO.OUT)              # 각 LED핀을 출력모드로 설정              
                             
-GPIO.output(led_red,False)
+GPIO.output(led_red,False)                 # 일단 모든 LED OFF
 GPIO.output(led_green,False)
 GPIO.output(led_blue,False)
 
 try:
-    for i in range(10):
+    for i in range(10):                    # 10번 ON-OFF 반복
         for pin in color_led:
-            GPIO.output(pin, GPIO.HIGH)
-            time.sleep(0.5)
-        for pin in color_led:
-            GPIO.output(pin, GPIO.LOW)
+            GPIO.output(pin, GPIO.HIGH)    # LED ON
+            time.sleep(0.5)                # 0.5초 기다림
+        for pin in color_led:              
+            GPIO.output(pin, GPIO.LOW)     # LED OFF
         
 except KeyboardInterrupt:
     pass
-GPIO.cleanup()
+GPIO.cleanup()                             # GPIO 설정 초기화
     
