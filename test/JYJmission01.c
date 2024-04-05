680c3a6d81bd7111e6b50d457db1944e186b5215
yeju jo <154478990+coco-mango@users.noreply.github.com>
2024-04-05 15:37:22 +0900

    Update JYJmission01.c

--- test/JYJmission01.c
+++ test/JYJmission01.c
@@ -1,26 +1,26 @@
-#include <wiringPi.h>
+#include <wiringPi.h>                    // 라즈베리파이의 GPIO(General Purpose Input Output)핀에 엑세스 하기 위한 C/C++ 라이브러리
 #include <stdio.h>
 
-#define led_red 7
-#define led_green 21
-#define led_blue 22
+#define led_red 7                        // RED LED의 GPIO 핀 번호 정의
+#define led_green 21                     // GREEN LED의 GPIO 핀 번호 정의
+#define led_blue 22                      // BLUE LED의 GPIO 핀 번호 정의
 
 int main(void){
-    if(wiringPiSetup() == -1)
+    if(wiringPiSetup() == -1)           //Wiringpi 라이브러리 초기화에 실패하면(-1) 1을 반환
 		return 1;
 
-    pinMode(led_red, OUTPUT);
-    pinMode(led_green, OUTPUT);
-    pinMode(led_blue, OUTPUT);
+    pinMode(led_red, OUTPUT);           // RED LED 핀을 출력모드로 설정
+    pinMode(led_green, OUTPUT);         // GREEN LED 핀을 출력모드로 설정
+    pinMode(led_blue, OUTPUT);          // BLUE LED 핀을 출력모드로 설정
     
-    for(int i=0;i<10;i++){
-        digitalWrite(led_red,HIGH);
-        digitalWrite(led_green,HIGH);
-        digitalWrite(led_blue,HIGH);
-        delay(1000);
-        digitalWrite(led_red,LOW);
-        digitalWrite(led_green,LOW);
-        digitalWrite(led_blue,LOW);
+    for(int i=0;i<10;i++){              // LED ON-OFF 10번 반복
+        digitalWrite(led_red,HIGH);     // RED LED ON
+        digitalWrite(led_green,HIGH);   // GREEN LED ON
+        digitalWrite(led_blue,HIGH);    // BLUE LED ON
+        delay(1000);                    // 1000밀리초(=1초) 기다림
+        digitalWrite(led_red,LOW);      // RED LED OFF
+        digitalWrite(led_green,LOW);    // GREEN LED OFF
+        digitalWrite(led_blue,LOW);     // BLUE LED OFF
         delay(1000);
     }
     
